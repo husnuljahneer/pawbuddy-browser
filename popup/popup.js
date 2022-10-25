8ca8f0e405ff5a671ec7a23ef8d4fc884ebb5ef9
husnul <husnuljahneer19@gmail.com>
2022-10-25 22:50:32 +0530

    Fixed Multiple Pets coming up

--- popup/popup.js
+++ popup/popup.js
@@ -11,10 +11,16 @@ document.addEventListener("DOMContentLoaded", function () {
           var currentTab = tabs.filter((tab) => tab.active)[0];
           chrome.tabs.update(tab.id, { active: true });
           function appendImage() {
+            if (document.getElementById("bat_image")) {
+              return;
+            }
+            if (document.getElementById("crocs_image")) {
+              document.getElementById("crocs_image").remove();
+            }
             let img = document.createElement("img");
+            img.setAttribute("id", "bat_image");
             img.draggable = false;
-            img.position = "absolute";
-            img.style.position = "absolute";
+            img.style.position = "fixed";
             img.style.top = "100px";
             img.style.left = "40%";
             img.style.zIndex = "99";
@@ -27,6 +33,40 @@ document.addEventListener("DOMContentLoaded", function () {
             {
               target: { tabId: tab.id },
               func: appendImage
+            },
+            (injectionResults) => {
+              console.log("done", injectionResults);
+            }
+          );
+        }
+      );
+      return tab;
+    }
+  });
+
+  const clearButton = document.getElementById("clear_button");
+  clearButton.addEventListener("click", function () {
+    getCurrentTab();
+    async function getCurrentTab() {
+      let queryOptions = { active: true, lastFocusedWindow: true };
+      let [tab] = await chrome.tabs.query(queryOptions);
+      chrome.tabs.query(
+        { windowId: chrome.windows.WINDOW_ID_CURRENT },
+        (tabs) => {
+          var currentTab = tabs.filter((tab) => tab.active)[0];
+          chrome.tabs.update(tab.id, { active: true });
+          function removeImage() {
+            if (document.getElementById("bat_image")) {
+              document.getElementById("bat_image").remove();
+            }
+            if (document.getElementById("crocs_image")) {
+              document.getElementById("crocs_image").remove();
+            }
+          }
+          chrome.scripting.executeScript(
+            {
+              target: { tabId: tab.id },
+              func: removeImage
             },
             (injectionResults) => {
               console.log("done", injectionResults);
@@ -51,10 +91,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
           chrome.tabs.update(tab.id, { active: true });
           function appendImage() {
+            if (document.getElementById("crocs_image")) {
+              return;
+            }
+            if (document.getElementById("bat_image")) {
+              document.getElementById("bat_image").remove();
+            }
             let img = document.createElement("img");
+            img.setAttribute("id", "crocs_image");
             img.draggable = false;
-            img.position = "absolute";
-            img.style.position = "absolute";
+            img.style.position = "fixed";
             img.style.top = "100px";
             img.style.left = "40%";
             img.style.zIndex = "99";
