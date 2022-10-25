488572d5eeb6f18fc3e5035ddd9dcc39b136a506
husnul <husnuljahneer19@gmail.com>
2022-10-26 01:34:44 +0530

    Moving pets now

--- popup/popup.js
+++ popup/popup.js
@@ -44,6 +44,78 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  const moveThePetButton = document.getElementById("move_the_pets");
+  moveThePetButton.addEventListener("click", function () {
+    getCurrentTab();
+    async function getCurrentTab() {
+      let queryOptions = { active: true, lastFocusedWindow: true };
+      let [tab] = await chrome.tabs.query(queryOptions);
+      chrome.tabs.query(
+        { windowId: chrome.windows.WINDOW_ID_CURRENT },
+        (tabs) => {
+          var currentTab = tabs.filter((tab) => tab.active)[0];
+          chrome.tabs.update(tab.id, { active: true });
+          function moveThePet() {
+            if (document.getElementById("bat_image")) {
+              let img = document.getElementById("bat_image");
+              img.style.top = "100px";
+              let left = 0;
+              let speed = 1;
+              let top = 100;
+              setTimeout(() => {
+                let interval = setInterval(() => {
+                  if (left >= 100) {
+                    speed = -1;
+                  } else if (left <= 0) {
+                    speed = 1;
+                  }
+                  left += speed;
+                  top += speed;
+                  img.style.left = left + "%";
+                  img.style.top = top + "px";
+                }, 60);
+              }, 60);
+            }
+            if (document.getElementById("crocs_image")) {
+              let img = document.getElementById("crocs_image");
+              img.style.top = "100px";
+              let left = 0;
+              let speed = 1;
+              setTimeout(() => {
+                let interval = setInterval(() => {
+                  if (left >= 100) {
+                    speed = -1;
+                  } else if (left <= 0) {
+                    speed = 1;
+                  }
+                  left += speed;
+                  top += speed;
+                  img.style.left = left + "%";
+                  if (left >= 100) {
+                    img.style.transform = "scaleX(1)";
+                  }
+                  if (left <= 1) {
+                    img.style.transform = "scaleX(-1)";
+                  }
+                }, 60);
+              }, 60);
+            }
+          }
+          chrome.scripting.executeScript(
+            {
+              target: { tabId: tab.id },
+              func: moveThePet
+            },
+            (injectionResults) => {
+              console.log("done", injectionResults);
+            }
+          );
+        }
+      );
+      return tab;
+    }
+  });
+
   const clearButton = document.getElementById("clear_button");
   clearButton.addEventListener("click", function () {
     getCurrentTab();
@@ -100,9 +172,10 @@ document.addEventListener("DOMContentLoaded", function () {
             let img = document.createElement("img");
             img.setAttribute("id", "crocs_image");
             img.draggable = false;
+            img.style.transform = "scaleX(-1)";
             img.style.position = "fixed";
             img.style.top = "100px";
-            img.style.left = "40%";
+            img.style.left = "45%";
             img.style.zIndex = "99";
             img.width = 150;
             img.height = 150;
